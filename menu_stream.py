f244e720b87dc6b5555bff3c96904e71b0a6f63f
Suvin Koshy George <70506817+skfreego@users.noreply.github.com>
2023-05-17 11:59:07 +0530

    Update menu_stream.py

--- menu_stream.py
+++ menu_stream.py
@@ -443,7 +443,7 @@ def display_map(df):
     # Display the updated results
 
     st.subheader("Live map of locations")
-    folium_static(m, width=1000, height=1000)
+    folium_static(m, width=950, height=1100)
 
     st.subheader(f"1.Total Records: {int(total_records1)}")
 
